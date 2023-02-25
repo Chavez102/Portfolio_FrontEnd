586714d452ab68ca17aaf1da8fd0611b1cc4c4f4
Bryan Chavez <bryanchavez102@gmail.com>
2023-02-25 11:51:02 -0500

    prduction url

--- Portfolio/src/environments/environment.prod.ts
+++ Portfolio/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   withCredentials: true,
-  baseUrl: "http://18.216.6.243:8081", 
+  baseUrl: "http://18.118.173.1:8081/", 
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': "http://localhost:4200",
